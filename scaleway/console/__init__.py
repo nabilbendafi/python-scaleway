d70c5acefe2cc04dc4b12d6dbe3f78ae11adf5bc
Nabil BENDAFI <nabil@bendafi.fr>
2018-10-29 14:24:10 +0100

    CLI banner

--- scaleway/console/__init__.py
+++ scaleway/console/__init__.py
@@ -6,3 +6,12 @@
 # Licensed under the BSD 2-Clause License (the "License"); you may not use this
 # file except in compliance with the License. You may obtain a copy of the
 # License at https://opensource.org/licenses/BSD-2-Clause
+import sys
+
+__version__ = '0.0.1'
+
+if sys.version_info < (3,):
+    raise ImportError(
+        '''
+        Scaleway python CLI {0} is Python3 compatible only.
+        '''.format(__version__))
--- scaleway/console/cli.py
+++ scaleway/console/cli.py
@@ -8,12 +8,32 @@
 # file except in compliance with the License. You may obtain a copy of the
 # License at https://opensource.org/licenses/BSD-2-Clause
 
+import click
+
+from .__init__ import __version__
 from prompt_toolkit import prompt
 
 
+@click.group()
 def cli():
-    answer = prompt('Give me some input: ')
-    print('You said: %s' % answer)
+    """
+        \b
+                       _
+         ___  ___ __ _| | _____      ____ _ _   _
+        / __|/ __/ _` | |/ _ \ \ /\ / / _` | | | |
+        \__ \ (_| (_| | |  __/\ V  V / (_| | |_| |
+        |___/\___\__,_|_|\___| \_/\_/ \__,_|\__, |
+                                            |___/
+                                        Python CLI
+        \b
+    """
+    pass
+
+
+@cli.command()
+def version():
+    """Prints the current version and exists."""
+    click.secho('Scaleway python CLI: {0}'.format(__version__))
 
 
 if __name__ == '__main__':
